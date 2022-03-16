674946f153f9b44162735d77bc53bc49b96805e2
Natanael Freire <natanael.l.freire@gmail.com>
2022-03-16 18:40:47 -0300

    remove # into private property

--- src/configuration.js
+++ src/configuration.js
@@ -4,20 +4,20 @@ const homedir = require('os').homedir()
 const path = require('path')
 
 export class Configuration {
-  #DURATION_DEFAULT = 86400
-  #PROFILE_DEFAULT = 'default'
-  #REGION_DEFAULT = 'us-east-1'
-  #ourPath
-  #ourConfigFile
+  DURATION_DEFAULT = 86400
+  PROFILE_DEFAULT = 'default'
+  REGION_DEFAULT = 'us-east-1'
+  ourPath
+  ourConfigFile
 
   constructor () {
     const ourFolder = '.aws-mfa'
-    this.#ourPath = path.join(homedir, ourFolder)
-    this.#ourConfigFile = path.join(this.#ourPath, 'config.json')
+    this.ourPath = path.join(homedir, ourFolder)
+    this.ourConfigFile = path.join(this.ourPath, 'config.json')
   }
 
   async configure (options) {
-    const params = await this.#gerParams()
+    const params = await this.gerParams()
     if (!params.serialNumber) {
       console.error('serialNumber is required')
       return
@@ -26,17 +26,17 @@ export class Configuration {
     const profile = params.profile
     delete params.profile
     currentConfiguration[profile] = { ...params }
-    this.#storeConfiguration(currentConfiguration)
+    this.storeConfiguration(currentConfiguration)
   }
 
-  async #storeConfiguration (configuration) {
-    if (!fs.existsSync(this.#ourPath)) fs.mkdirSync(this.#ourPath)
-    fs.writeFileSync(this.#ourConfigFile, JSON.stringify(configuration, null, 2), { encoding: 'utf8' })
+  async storeConfiguration (configuration) {
+    if (!fs.existsSync(this.ourPath)) fs.mkdirSync(this.ourPath)
+    fs.writeFileSync(this.ourConfigFile, JSON.stringify(configuration, null, 2), { encoding: 'utf8' })
   }
 
   getConfiguration (profile) {
     try {
-      const config = JSON.parse(fs.readFileSync(this.#ourConfigFile, 'utf-8'))
+      const config = JSON.parse(fs.readFileSync(this.ourConfigFile, 'utf-8'))
       if (!profile) return config
       return config[profile]
     } catch (error) {
@@ -44,7 +44,7 @@ export class Configuration {
     }
   }
 
-  async #gerParams (options) {
+  async gerParams (options) {
     const questions = [
       {
         type: 'input',
@@ -55,19 +55,19 @@ export class Configuration {
         type: 'input',
         name: 'profile',
         message: 'Profile: ',
-        default: this.#PROFILE_DEFAULT
+        default: this.PROFILE_DEFAULT
       },
       {
         type: 'number',
         name: 'duration',
         message: 'Duration (in seconds):',
-        default: this.#DURATION_DEFAULT
+        default: this.DURATION_DEFAULT
       },
       {
         type: 'input',
         name: 'region',
         message: 'Region:',
-        default: this.#REGION_DEFAULT
+        default: this.REGION_DEFAULT
       }
     ]
 
